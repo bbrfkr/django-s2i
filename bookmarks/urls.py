98926dce286fd7b05edee64ba7658de64674990a
bbrfkr <bbrfkr@gmail.com>
2018-11-20 18:12:57 +0900

    folders

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:folder_id>/', views.contents, name='contents'),
+    path('bookmarks/', views.index, name='index'),
+    path('bookmarks/<int:folder_id>/', views.contents, name='contents'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+import bookmarks
 urlpatterns = [
-    path('', bookmarks.views.index),
-    path('bookmarks/', include('bookmarks.urls')),
+    path('', include('bookmarks.urls')),
     path('admin/', admin.site.urls),
 ]
